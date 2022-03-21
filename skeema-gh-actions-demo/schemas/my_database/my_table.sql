e0839e152365c7331b4216ff4bbadd4ea604461a
Steven Aldinger <stevenaldinger@gmail.com>
2022-03-20 20:22:20 -0400

    feat: update my_table schema

--- skeema-gh-actions-demo/schemas/my_database/my_table.sql
+++ skeema-gh-actions-demo/schemas/my_database/my_table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `my_table` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `birthday` datetime NOT NULL,
   `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `favorite_color` enum('red','green','blue','purple') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
